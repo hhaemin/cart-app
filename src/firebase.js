977107ca474a9d68a2cf3650eee26f79ec023489
hhaemin <81980221+hhaemin@users.noreply.github.com>
2024-07-17 16:13:23 +0800

    Update firebase.js- apikey 삭제

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,14 @@ import { getAuth } from "firebase/auth";
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
-  apiKey: "AIzaSyB4j06yS2lrarn6P7o58Z9LAWOwlXQI0pM",
-  authDomain: "cart-app-899fc.firebaseapp.com",
+  apiKey: "",
+  authDomain: "",
   databaseURL:
-    "https://cart-app-899fc-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "cart-app-899fc",
-  storageBucket: "cart-app-899fc.appspot.com",
-  messagingSenderId: "607534428607",
-  appId: "1:607534428607:web:4181f4b7b2e1d82f52b10a",
+    "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 // Initialize Firebase
